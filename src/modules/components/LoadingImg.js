887e454ce46c4d5c66a42683d0489092df8f5c6d
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-01-07 10:46:24 +0200

    new animation pulsating added

--- src/modules/components/LoadingImg.js
+++ src/modules/components/LoadingImg.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { CircularProgress, LinearProgress } from '@mui/material/';
-import { styled } from '@mui/system';
+import { styled, keyframes } from '@mui/system';
+
+const pulsate = keyframes`
+  0% {
+    transform: scale(0.5);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
 
 const AnimatedLogo = styled('img')({
   width: '100px',
   borderRadius: '50px',
-  animation: '$spin 2s infinite linear',
+  animation: `${pulsate} 1s infinite alternate`,
 });
 
 function LoadingImg() {
-  const [deg, setDeg] = useState(0);
+  //   const [deg, setDeg] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDeg((prev) => (prev + 5) % 360);
-    }, 10);
+  //   useEffect(() => {
+  //     const interval = setInterval(() => {
+  //       setDeg((prev) => (prev + 5) % 360);
+  //     }, 10);
 
-    return () => clearInterval(interval);
-  }, []);
+  //     return () => clearInterval(interval);
+  //   }, []);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -28,7 +40,6 @@ function LoadingImg() {
 
     return () => clearTimeout(timer);
   }, []);
-
   const logo = '/apple-touch-icon.png';
   return (
     <div
@@ -39,11 +50,7 @@ function LoadingImg() {
         height: '100vh',
       }}
     >
-      <AnimatedLogo
-        style={{ transform: `rotate(${deg}deg)` }}
-        src={logo}
-        alt="logo"
-      />
+      <AnimatedLogo style={{ transform: `scale(0.5)` }} src={logo} alt="logo" />
     </div>
   );
 }
